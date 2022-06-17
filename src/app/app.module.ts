97b89ae150d08ad11d369051b1aa8158bdb59ce2
Rion Kaku <rionkaku119@gmail.com>
2022-06-18 00:30:21 +0900

    route_strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRouteReuseStrategy } from './appRouteReuseStrategy';
 import { PageModule } from './page/page.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, PageModule, AppRoutingModule],
-  providers: [],
+  providers: [{ provide: RouteReuseStrategy, useClass: AppRouteReuseStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
